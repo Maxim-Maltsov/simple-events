b7be7c650bd320343d6dda25b30e8cb79ee47810
Maxim-Maltsov <maltsovm@gmail.com>
2022-05-06 14:33:40 +0700

    second commit

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -30,30 +30,42 @@ return [
 
     'connections' => [
 
+        // Pusher settings
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
-            // 'options' => [
-            //     'cluster' => env('PUSHER_APP_CLUSTER'),
-            //     'useTLS' => true,
-            // ],
-
-            // Websockets options
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'http'
+                'useTLS' => true,
             ],
-
-
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
         ],
+
+
+        // Websockets settings
+        // 'pusher' => [
+
+        //     'driver' => 'pusher',
+        //     'key' => env('PUSHER_APP_KEY'),
+        //     'secret' => env('PUSHER_APP_SECRET'),
+        //     'app_id' => env('PUSHER_APP_ID'),
+        //     'options' => [
+        //         'cluster' => env('PUSHER_APP_CLUSTER'),
+        //         'encrypted' => true,
+        //         'host' => '127.0.0.1',
+        //         'port' => 6001,
+        //         'scheme' => 'http'
+        //     ],
+        //     'client_options' => [
+        //         // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+        //     ],
+        // ],
+
+
 
         'ably' => [
             'driver' => 'ably',
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -20,18 +20,22 @@ import Echo from 'laravel-echo';
 
 window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
 
+// Настройки для Pusher
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: 'local',
-    wsHost: 'localhost',
-    wsPort: 6001,
-    forceTLS: false,
-    disableStats: true,
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    forceTLS: true
 });
+
+
+// Настройки для Webspckets(локальные)
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: 'local',
+//     wsHost: 'localhost',
+//     wsPort: 6001,
+//     forceTLS: false,
+//     disableStats: true,
+// });
